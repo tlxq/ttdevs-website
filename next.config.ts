0c5a85982ed51081e6f9db9b34273ec2522c41df
Tom Larsson <tlxqz@proton.me>
2026-01-06 13:07:53 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const repo = "ttdevs-website";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-
-  // Needed for GitHub Pages project site:
-  // https://tlxq.github.io/ttdevs-website/
   basePath: `/${repo}`,
   assetPrefix: `/${repo}/`,
 };
